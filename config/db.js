3193b853cb94ac3f1ab2c4dcc7b55d4ca24d54b9
Sanjana M Patil <sanjanampatil6@gmail.com>
2026-05-19 23:49:30 +0530

    Given acces to any one for create deb

--- config/db.js
+++ config/db.js
@@ -6,8 +6,9 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 
 module.exports = pool
